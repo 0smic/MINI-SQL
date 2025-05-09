463c9e1530fd6664a855d8f91d75caecb5745912
Gokul <146622216+0smic@users.noreply.github.com>
2025-05-09 14:25:56 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "parser.h"
 
 #define INPUT_BUFFER 1024
-void parser_command(const char *input);
-void create_table(const char *input);
 void cl_loop();
 
 int main(){
@@ -28,25 +27,7 @@ void cl_loop(){
     
 }
 
-void parser_command(const char *input){
-    if (strncmp(input,"CREATE TABLE", 12) == 0){
-        create_table(input);
-    }else if (strncmp(input,"SELECT", 5) == 0){
-        printf("working");
-    }else if (strncmp(input,"INSERT INTO", 11) == 0){
-        printf("working");
-    }
-    
-}
-void create_table(const char *input){
-    char table_name[100];
-    char column[300];
-    if (sscanf(input, "CREATE TABLE %s (%[^)])",table_name,column) ==2){
-        printf("\n%s  --  %s\n",table_name,column);
-    }else {
-        printf("Syntax ERROR!!\n");
-        printf("CREATE TABLE table_name column\n");
-        printf("column: (column_name datatype, ....)\n");
-    }
+
+  
 }
 
