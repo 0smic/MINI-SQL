0c160d223d8dd9de1ba03bf0ba861d044d349b07
Gokul <146622216+0smic@users.noreply.github.com>
2025-05-09 15:54:37 +0530

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -3,4 +3,8 @@
   void parser_command(const char *input);
   void create_table(const char *input);
   void select_func(const char *input);
+  void delete_func(const char *input);
+  void insert_into(const char *input);
+  void update_func(const char *input);
+  void drop_table(const char *input);
 #endif
