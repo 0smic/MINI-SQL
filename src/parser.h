3b362bf2aba64cb681caaf70feb1be58a4969dde
Gokul <146622216+0smic@users.noreply.github.com>
2025-05-14 01:09:11 +0530

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -1,5 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
+  #define MAX_COLUMNS 10
+  #define MAX_ROWS 100
   void parser_command(const char *input);
   void create_parser(const char *input);
   void select_parser(const char *input);
@@ -7,4 +9,5 @@
   void insert_into_parser(const char *input);
   void update_parser(const char *input);
   void drop_parser(const char *input);
+  int sep_column_datatype(char ***column_name, char ***column_datatype, char *column);
 #endif
