f530125bc553238a560f8fee3d2397bd44ecdc03
Gokul <146622216+0smic@users.noreply.github.com>
2025-05-17 01:37:26 +0530

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -1,5 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
+  #include "datatype.h"
   #define MAX_COLUMNS 10
   #define MAX_ROWS 100
   void parser_command(const char *input);
@@ -10,4 +11,5 @@
   void update_parser(const char *input);
   void drop_parser(const char *input);
   int sep_column_datatype(char ***column_name, char ***column_datatype, char *column);
+  DataType get_data_type_from_string(const char *type_str);
 #endif
