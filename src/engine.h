4b69e5556a1c07c96e20fc4ed0a225827aadf87c
Gokul <146622216+0smic@users.noreply.github.com>
2025-05-17 01:34:28 +0530

    Update engine.h

--- src/engine.h
+++ src/engine.h
@@ -1,5 +1,8 @@
 #ifndef ENGINE_H
 #define ENGINE_H
+  #include "datatype.h"
   typedef struct table table_t;
-  table_t *create_table(const char *table_name, const char *columns);
+  union Data;
+  int hash(const char *key);
+  void create_table(const char *table_name, const char *columns);
 #endif // ENGINE_H
